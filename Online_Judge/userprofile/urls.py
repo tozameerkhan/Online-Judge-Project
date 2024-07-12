79b0290c0ee8eb57d54563646031e4a757a374ec
Zameer Khan <tozameerkhan@gmail.com>
2024-07-12 22:41:35 +0530

    Removed

--- Online_Judge/userprofile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from userprofile.views import update_profile
-
-app_name = 'userprofile'  
-
-urlpatterns = [
-    #path("profile/", profiles,name = "user_profile"),
-    path("profile/", update_profile, name='update_profile'),
-]
